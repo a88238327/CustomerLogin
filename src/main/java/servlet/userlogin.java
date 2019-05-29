8c2c061c42ad352f28754464beadcbc2b4b0bd06
a88238327 <540372951@qq.com>
2019-05-29 17:39:03 +0800

    maven

--- src/main/java/servlet/userlogin.java
+++ src/main/java/servlet/userlogin.java
@@ -20,8 +20,8 @@ import net.sf.json.JSONObject;
  */
 @WebServlet("/userlogin")
 public class userlogin extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -30,45 +30,53 @@ public class userlogin extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf8");
-		response.setCharacterEncoding("utf8");
-		if(request.getParameter("code")==null)
-		{
-			response.sendRedirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxafe6999b4d77754a&redirect_uri=https://cloud.hnjtbf.com/CustomerLogin/userlogin&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect");
-		}
-		else {
-			String code=request.getParameter("code");
-			System.out.println(code);
-			String result=Util.get("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxafe6999b4d77754a&secret=85642ddb58fc2fc7b1c52720e298485e&code="+code+"&grant_type=authorization_code ");
-			JSONObject jsonObject=JSONObject.fromObject(result);
-			System.out.println(jsonObject.getString("openid"));
-			//通过openid查询该用户是否注册
-			String openid=jsonObject.getString("openid");
-			String phone=selectdata.getphone(openid);
-			HttpSession session=request.getSession();
-			if(!phone.equals(""))
-			{
-				session.setAttribute("phone", phone);
-				session.setAttribute("openid", openid);
-				response.sendRedirect("shouye.html");
-			}
-			else {
-				response.sendRedirect("zhuce.html");
-			}
-		}
-		
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf8");
+        response.setCharacterEncoding("utf8");
+        if (request.getParameter("code") == null) {
+            response.sendRedirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxafe6999b4d77754a&redirect_uri=https://cloud.hnjtbf.com/CustomerLogin/userlogin&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect");
+        } else {
+            String code = request.getParameter("code");
+            System.out.println(code);
+            String result = Util.get("https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxafe6999b4d77754a&secret=85642ddb58fc2fc7b1c52720e298485e&code=" + code + "&grant_type=authorization_code ");
+            JSONObject jsonObject = JSONObject.fromObject(result);
+            System.out.println(jsonObject.getString("openid"));
+            //通过openid查询该用户是否注册
+            String openid = jsonObject.getString("openid");
+            String phone = selectdata.getphone(openid);
+            HttpSession session = request.getSession();
+            if (!phone.equals("")) {
+                session.setAttribute("phone", phone);
+                session.setAttribute("openid", openid);
+                String go = request.getParameter("go");
+                if (go != null) {
+                    if (go.equals("buybaoxian")) {
+                        response.sendRedirect("https://cloud.hnjtbf.com/CustomerLogin/cars.html");
+                    } else if (go.equals("mianfeinianshen")) {
+                        response.sendRedirect("https://cloud.hnjtbf.com/CustomerLogin/shenqingchoose.html?servicename=%E5%85%8D%E8%B4%B9%E5%B9%B4%E5%AE%A1");
+                    }
+                } else {
+                    response.sendRedirect("shouye.html");
+                }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+
+            } else {
+                response.sendRedirect("zhuce.html");
+            }
+        }
+
+
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        doGet(request, response);
+    }
 
 }
